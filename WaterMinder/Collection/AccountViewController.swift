7153475118723794ce1c45292200cfe7d618b5cd
YuliaSimanavets <mrs.simonovets@gmail.com>
2022-12-05 16:53:01 +0100

    add TabBar, Controllers

--- WaterMinder/Collection/AccountViewController.swift
+++ WaterMinder/Collection/AccountViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  AccountViewController.swift
 //  WaterMinder
 //
 //  Created by Yuliya on 05/12/2022.
@@ -7,13 +7,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class AccountViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .lightGray
+
     }
 
-
 }
-
---
+++ WaterMinder/Collection/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  WaterMinder
+//
+//  Created by Yuliya on 05/12/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .lightGray
+        
+    }
+
+
+}
+
